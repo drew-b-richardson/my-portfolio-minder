6386eef823d76070fd97ee731aaa3676ea43576e
andrew richardson <drew.b.richardson@gmail.com>
2010-11-28 08:54:22 -0500

    initial checkin of skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = '72199ea116130850c3c151c750f1655a6220eb1c8352bb95dafc7781b4e004f7fa76470821a38614df80b40b9658a3fb64f5c726c00abfaeb5c82e41ec13ab94'
